0efc6e66bd88d1de3597da58e9d72acaa2a38739
NiJeTi <nijeti.inc@gmail.com>
2020-09-19 19:45:33 +0300

    Hello API controller

---
+++ LoggerTest/Controllers/HelloController.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+using LoggerTest.Services;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoggerTest.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HelloController : Controller
+    {
+        private readonly NameHolder _nameHolder;
+
+        public HelloController(NameHolder nameHolder)
+        {
+            _nameHolder = nameHolder;
+        }
+
+        [HttpPost("set_name")]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult SetName([FromQuery] string name)
+        {
+            if (!name.All(char.IsLetterOrDigit))
+                return BadRequest();
+
+            _nameHolder.Name = name;
+
+            return Accepted();
+        }
+
+        [HttpGet("hello")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Hello() => Ok($"Hello, {_nameHolder.Name}!");
+    }
+}
