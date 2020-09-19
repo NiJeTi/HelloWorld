00faba21f40a1e80125ad3c8a3a3d9b7ac532f72
NiJeTi <nijeti.inc@gmail.com>
2020-09-19 19:43:46 +0300

    Mini-storage for last user name

---
+++ LoggerTest/Services/NameHolder.cs
@@ -0,0 +1,12 @@
+﻿namespace LoggerTest.Services
+{
+    public class NameHolder
+    {
+        public NameHolder(string name)
+        {
+            Name = name;
+        }
+        
+        public string Name { get; set; }
+    }
+}
